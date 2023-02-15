f86acb08cf91aa628c9efafd25ddf4833d56ef22
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-02-15 19:42:52 +0530

    Center App using CSS and write functions for

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	return <div className="App">React</div>;
+	const [count, setCount] = useState(0);
+
+	const handleDecrement = () => {
+		if (count === 0) return;
+		setCount(count - 1);
+	};
+
+	const handleIncrement = () => {
+		if (count === 10) return;
+		setCount(count + 1);
+	};
+
+	return (
+		<div className="App">
+			<div className="container">
+				<button onClick={handleDecrement}>-</button>
+				<h1>{count}</h1>
+				<button onClick={handleIncrement}>+</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
